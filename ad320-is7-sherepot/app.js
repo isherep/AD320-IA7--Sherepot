400d17025d6e99c13eda701a5c44d71cc5386ba7
Iryna <iryna.sherepot@gmail.com>
2019-03-02 11:05:36 -0800

    json object created

--- ad320-is7-sherepot/app.js
+++ ad320-is7-sherepot/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter   = require('./routes/users');
 var route1Router  = require('./routes/route1');
+var route2Router  = require('./routes/route2');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/route1',route1Router);
+app.use('/route2', route2Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ ad320-is7-sherepot/routes/route2.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+var object = require('../colors.json');
+/*Get route1 page*/
+console.log(object);
+
+router.get('/route2', function(req, res, next){
+  res.send(object);
+});
+
+module.exports = router;
